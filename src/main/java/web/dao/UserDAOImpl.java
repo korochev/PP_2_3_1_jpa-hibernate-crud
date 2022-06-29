ff2676936f8c34006dbf10941bcf037dfbe4646b
korochev <myver33@gmail.com>
2022-06-29 11:49:28 +0300

    intellij_idea//2022.1//29.06

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -1,8 +1,6 @@
 package web.dao;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import web.models.User;
 
 import javax.persistence.EntityManager;
@@ -15,24 +13,28 @@ public class UserDAOImpl implements UserDAO {
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Override
     public List<User> index() {
         return entityManager.createQuery("select u from User u", User.class)
                 .getResultList();
     }
 
+    @Override
     public User show(int id) {
         return entityManager.find(User.class, id);
     }
 
+    @Override
     public void save(User User) {
         entityManager.persist(User);
     }
 
-
+    @Override
     public void update(User updatedUser) {
         entityManager.merge(updatedUser);
     }
 
+    @Override
     public void delete(int id) {
         entityManager.createQuery("delete from User u where u.id = :id")
                 .setParameter("id", id)
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDAO;
